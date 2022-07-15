69a0a6b03068472763ec0d1d281e7b021be87b0c
Kaige Ye <ye@kaige.org>
2022-07-15 23:57:35 +0800

    init

---
+++ src/main/java/org/kaige/mysqldriver/MysqlDriverApplication.java
@@ -0,0 +1,13 @@
+package org.kaige.mysqldriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlDriverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlDriverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kaige/mysqldriver/MysqlDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kaige.mysqldriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlDriverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
